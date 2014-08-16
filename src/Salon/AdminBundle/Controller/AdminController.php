1e7ea581a04aa09266fba1473d256d49fe3f7a75
zwbeyond <zwbeyond@gmail.com>
2014-08-16 23:41:12 +0800

    first update

---
+++ src/Salon/AdminBundle/Controller/AdminController.php
@@ -0,0 +1,12 @@
+<?php  
+namespace Salon\AdminBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class AdminController extends Controller{
+	public function indexAction(){
+        return $this->render('SalonAdminBundle:Default:index.html.twig'sswe);
+    }
+}
+
+?>
---
+++ src/Salon/AdminBundle/SalonAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Salon\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SalonAdminBundle extends Bundle
+{
+}
